12585ec4c7e93db8118569e3b26aba1aa6ba9efd
Samir Medjdoub <samirmedjdoub@imac-de-samir-1.home>
2022-08-01 19:21:12 +0200

    Fix - Undo redo on task create/delete/drag + loading from server

--- src/MissionGanttConfig.js
+++ src/MissionGanttConfig.js
@@ -10,6 +10,23 @@ const ganttConfig = {
   tbar: {
     type: "gantttoolbar",
   },
+  project: {
+    transport: {
+      load: {
+        url: "http://localhost:3001/data",
+      },
+      sync: {
+        url: "http://localhost:3001/data",
+      },
+    },
+    autoLoad: true,
+    // The State TrackingManager which the UndoRedo widget in the toolbar uses
+    stm: {
+      autoRecord: true,
+    },
+    // Reset Undo / Redo after each load so that the first load from url does not count as an undoable task
+    resetUndoRedoQueuesAfterLoad: true,
+  },
   // to add filtering option on every column grid header
   filterFeature: true,
   taskMenuFeature: {
--- src/components/MissionGantt.js
+++ src/components/MissionGantt.js
@@ -1,26 +1,9 @@
-import { useRef, useEffect } from "react";
-import { BryntumGantt, BryntumProjectModel } from "@bryntum/gantt-react";
+import { BryntumGantt } from "@bryntum/gantt-react";
 import "../App.scss";
 
-import missionTasks from "./gantt-mars-data.js";
-
 function MissionGant(props) {
-  const ganttRef = useRef();
-  const projectRef = useRef();
-  const { tasks, dependencies } = missionTasks;
-  useEffect(() => {
-    const { project } = ganttRef.current.instance;
-    project.stm.enable();
-  }, []);
   return (
-    <>
-      <BryntumProjectModel
-        tasks={tasks}
-        dependencies={dependencies}
-        ref={projectRef}
-      />
-      <BryntumGantt {...props} ref={ganttRef} project={projectRef} />
-    </>
+      <BryntumGantt {...props} />
   );
 }
 
